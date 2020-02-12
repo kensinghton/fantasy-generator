9f61e3fbbca200c4d07d6722129d8fafe2cb1516
kensinghton <kensinghton.chump@gmail.com>
2020-02-12 20:42:13 +0200

    added races selection for village and races tensions

--- generator/scripts/villageMain.js
+++ generator/scripts/villageMain.js
@@ -21,6 +21,47 @@
 		villageRulerTitleSelected = villageRulerTitle[2];
 		break;
 	}
+	var villageRacesSelected = Math.floor(Math.random() * (villageRace.length));
+	var villageRacesTensionsRoll = Math.floor(Math.random() * 20 + 1);
+	switch (villageRacesTensionsRoll) {
+	  case 1:
+	  case 2:
+	  case 3:
+	  case 4:
+	  case 5:
+	  case 6:
+	  case 7:
+	  case 8:
+	  case 9:
+	  case 10:
+		villageRacesTensionsSelected = villageRacesTensions[0];
+		break;
+	  case 11:
+	  case 12:
+	  case 13:
+	  case 14:
+		villageRacesTensionsSelected = villageRacesTensions[1];
+		break;
+	  case 15:
+	  case 16:
+		villageRacesTensionsSelected = villageRacesTensions[2];
+		break;
+	  case 17:
+		villageRacesTensionsSelected = villageRacesTensions[3];
+		break;
+	  case 18:
+		villageRacesTensionsSelected = villageRacesTensions[4];
+		break;
+	  case 19:
+		villageRacesTensionsSelected = villageRacesTensions[5];
+		break;
+	  case 20:
+		villageRacesTensionsSelected = villageRacesTensions[6];
+		break;
+	}
+	document.getElementById('villageRace').innerHTML = villageRaces[villageRacesSelected];
+	document.getElementById('villageRace2').innerHTML = villageRaces[villageRacesSelected];
+	document.getElementById('villageTensions').innerHTML = villageRacesTensionsSelected;
 	document.getElementById('villageType').innerHTML = villageType;
 	document.getElementById('villagePopulation').innerHTML = villagePopulation;
  	//Village Ruler
